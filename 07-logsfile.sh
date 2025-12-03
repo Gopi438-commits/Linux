da206155f0f8bc3df9bae2890538da1c0cb8c37c
krishna <gopi@gmail.com>
2025-12-03 16:34:41 +0530

    session-03

--- 07-logsfile.sh
+++ 07-logsfile.sh
@@ -1,55 +1,58 @@
 #!/bin/bash
-
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-USERID=$(id -u)
+LOGS_FOLDER="/var/log/Linux"
+SCRIPT_NAME=$( echo $0 |cut -d '.'-f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: please run this script with root privelege"
     exit 1
 fi
-LOGS_FOLDER=/var/log/Linux
-SCRIPT_NAME=$(echo "$0 |cut -d '.'-f1")
-Log_file=$LOGS_FOLDER/$SCRIPT_NAME.log
+
 mkdir -p LOGS_FOLDER
+echo "Script started executed at: $(date)"
 
 VALIDATE(){
 
   if [ $1 -ne 0]; then
-       echo  -e "Installing $2 ... $R failure $N" &>> Log_file
+       echo  -e "Installing $2 ... $R failure $N" &>>$LOG_FILE
        exit 1
   else   
-       echo -e "Installing $2 ...$G SUCCESS $N"   &>> Log_file
+       echo -e "Installing $2 ...$G SUCCESS $N"   &>>$LOG_FILE
   fi     
 
 }
 
-dnf list installed mysql
+dnf list installed mysql  &>>$LOG_FILE
 #install if it is not found
 if [ $? -ne 0 ]; then
-  dnf install mysql -y     &>> Log_file
+  dnf install mysql -y     &>>$LOG_FILE
    VALIDATE $? "MYSQL"
 else 
    echo  -e "MQSQL  already exist ...$Y SKIPPING $N"
  fi  
    
 
-dnf list installed Nginx
+dnf list installed Nginx  &>>$LOG_FILE
 #install if it is not found
 if [ $? -ne 0 ]; then
-  dnf install nginx -y  &>> Log_file
+  dnf install nginx -y  &>>$LOG_FILE
    VALIDATE $? "Nginx"
 else 
    echo  -e "Nginx  already exist ...$Y SKIPPING $N"
  fi  
    
-dnf list installed python3
+dnf list installed python3 &>>$LOG_FILE
 #install if it is not found
 if [ $? -ne 0 ]; then
-  dnf install python3 -y  &>> Log_file
+  dnf install python3 -y  &>>$LOG_FILE
    VALIDATE $? "python3"
 else 
    echo -e "python3 already exist ...$Y SKIPPING $N"
