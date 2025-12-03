88a15f2b2f9d1d8eccbc79a543f50c772094dc6b
krishna <gopi@gmail.com>
2025-12-03 13:05:22 +0530

    session-03

---
+++ 06-colours.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run this script with root privelege"
+    exit 1
+fi
+
+VALIDATE(){
+
+  if [ $1 -ne 0]; then
+       echo  -e "Installing $2 ... $R failure $N"
+       exit 1
+  else   
+       echo -e "Installing $2 ...$G SUCCESS $N"
+  fi     
+
+}
+
+dnf list installed mysql
+#install if it is not found
+if [ $? -ne 0 ]; then
+  dnf install mysql -y
+   VALIDATE $? "MYSQL"
+else 
+   echo  -e "mysql already exist ...$Y SKIPPING $N"
+ fi  
+   
+
+dnf list installed Nginx
+#install if it is not found
+if [ $? -ne 0 ]; then
+  dnf install nginx -y
+   VALIDATE $? "nginx"
+else 
+   echo  -e "nginx  already exist ...$Y SKIPPING $N"
+ fi  
+   
+dnf list installed python3
+#install if it is not found
+if [ $? -ne 0 ]; then
+  dnf install python3 -y
+   VALIDATE $? "python3"
+else 
+   echo -e "python3 already exist ...$Y SKIPPING $N"
+ fi  
+   
