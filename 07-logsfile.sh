37fdfc3e01ff1e452d7f70c6421c1b21e45dd44f
krishna <gopi@gmail.com>
2025-12-03 16:23:55 +0530

    session-03

--- 07-logsfile.sh
+++ 07-logsfile.sh
@@ -12,17 +12,17 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 LOGS_FOLDER=/var/log/Linux
-SCRIPT_NAME=$(echo $0 |cut -d '.'-f1)
+SCRIPT_NAME=$(echo "$0 |cut -d '.'-f1")
 Log_file=$LOGS_FOLDER/$SCRIPT_NAME.log
 mkdir -p LOGS_FOLDER
 
 VALIDATE(){
 
   if [ $1 -ne 0]; then
-       echo  -e "Installing $2 ... $R failure $N" >>& Log_file
+       echo  -e "Installing $2 ... $R failure $N" &>> Log_file
        exit 1
   else   
-       echo -e "Installing $2 ...$G SUCCESS $N"   >>& Log_file
+       echo -e "Installing $2 ...$G SUCCESS $N"   &>> Log_file
   fi     
 
 }
@@ -30,7 +30,7 @@ VALIDATE(){
 dnf list installed mysql
 #install if it is not found
 if [ $? -ne 0 ]; then
-  dnf install mysql -y     >>& Log_file
+  dnf install mysql -y     &>> Log_file
    VALIDATE $? "MYSQL"
 else 
    echo  -e "MQSQL  already exist ...$Y SKIPPING $N"
@@ -40,7 +40,7 @@ else
 dnf list installed Nginx
 #install if it is not found
 if [ $? -ne 0 ]; then
-  dnf install nginx -y  >>& Log_file
+  dnf install nginx -y  &>> Log_file
    VALIDATE $? "Nginx"
 else 
    echo  -e "Nginx  already exist ...$Y SKIPPING $N"
@@ -49,7 +49,7 @@ else
 dnf list installed python3
 #install if it is not found
 if [ $? -ne 0 ]; then
-  dnf install python3 -y  >>& Log_file
+  dnf install python3 -y  &>> Log_file
    VALIDATE $? "python3"
 else 
    echo -e "python3 already exist ...$Y SKIPPING $N"
